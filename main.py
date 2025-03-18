e2cc2c0fb16b26f413796938e64f15c4e0afcf82
BogeticKarlo <kboget00@fesb.hr>
2025-03-18 17:05:19 +0100

    feat: dodana nova metoda

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
     print("Hello world!")
-    
+
 def nova_funkcija():
     return "Ovo je nova funkcija."
 
+def nova_metoda(self):
+    return "Ovo je nova metoda"
 
 if __name__ == "__main__":
     main()
